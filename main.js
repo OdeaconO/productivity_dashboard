6a9b0b886856825396e59682636b6e9638be6f6b
OdeaconO <dhruvrai2903@gmail.com>
2025-07-25 04:38:52 +0530

    not fun

--- main.js
+++ main.js
@@ -1,15 +1,45 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const isDev = require("electron-is-dev");
 
 function createWindow() {
-  const win = new BrowserWindow({
+  // Create the browser window
+  const mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
-    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      enableRemoteModule: false,
+    },
+    icon: path.join(__dirname, "assets/icon.png"), // Optional: if you have an icon
   });
 
-  win.loadFile(path.join(__dirname, "build", "index.html"));
-  win.webContents.openDevTools();
+  // Load the app
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+
+  // Open DevTools in development
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
 }
 
+// This method will be called when Electron has finished initialization
 app.whenReady().then(createWindow);
+
+// Quit when all windows are closed
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
