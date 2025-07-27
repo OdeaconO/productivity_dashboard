d88c484846a579988e3fde6c848ab1d03ba92ba1
OdeaconO <dhruvrai2903@gmail.com>
2025-07-27 15:10:28 +0530

    :)

--- main.js
+++ main.js
@@ -1,37 +1,43 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
-const isDev = require("electron-is-dev");
+
+// Simple check for development mode
+const isDev =
+  process.env.NODE_ENV === "development" ||
+  process.defaultApp ||
+  /[\\/]electron-prebuilt[\\/]/.test(process.execPath) ||
+  /[\\/]electron[\\/]/.test(process.execPath);
 
 function createWindow() {
-  // Create the browser window
   const mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    show: false,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
       enableRemoteModule: false,
     },
-    icon: path.join(__dirname, "assets/icon.png"), // Optional: if you have an icon
   });
 
-  // Load the app
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+    mainWindow.focus();
+  });
+
   mainWindow.loadURL(
     isDev
       ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
+      : `file://${path.join(__dirname, "build/index.html")}`
   );
 
-  // Open DevTools in development
   if (isDev) {
     mainWindow.webContents.openDevTools();
   }
 }
 
-// This method will be called when Electron has finished initialization
 app.whenReady().then(createWindow);
 
-// Quit when all windows are closed
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
